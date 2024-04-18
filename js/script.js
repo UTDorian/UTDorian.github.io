514f8b17649c36f60faa0bfe49843fcb635924c5
ShorpAzha <151829694+ShorpAzha@users.noreply.github.com>
2024-04-18 20:59:08 +0200

    let's go

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 let largeurEcran = window.innerWidth;
+let larg = largeurEcran / 2 - 200
 document.documentElement.style.setProperty('--largeur-ecran', `${largeurEcran}px`);
+document.documentElement.style.setProperty('--largeur', `${larg}px`);
